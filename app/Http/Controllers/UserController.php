0134dd903c255f4a3e5ca17299cc5354efbb7df0
NathanaelGracedo <nathanjuan56@gmail.com>
2025-03-03 18:09:01 +0700

    Jobsheet 4 - Praktikum 1 -

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,18 +9,13 @@ use Illuminate\Support\Facades\Hash;
 class UserController extends Controller
 {
     public function index() {
-        // $data = [
-        //     'username' => 'customer-1',
-        //     'nama' => 'Pelanggan',
-        //     'password' => Hash::make('12345'),
-        //     'level_id' => 3
-        // ];
-        // UserModel::insert($data);
-
         $data = [
-            'nama' => 'Pelanggan pertama',
+            'level_id' => 2,
+            'username' => 'manager_tiga',
+            'nama' => 'Manager 3',
+            'password' => Hash::make('12345'),
         ];
-        UserModel::where('username', 'customer-1')->update($data);
+        UserModel::create($data);
 
         $user = UserModel::all();
         return view('user', ['data' => $user]);
